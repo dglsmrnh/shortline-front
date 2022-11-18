61567ead90cd8af55d55450a319f456be194e222
PredoVin <pedro.vinicius1.pv@gmail.com>
2022-11-18 00:34:14 -0300

    Update Scan.js

--- src/views/scan/Scan.js
+++ src/views/scan/Scan.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 
 import { CContainer } from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import { cilArrowLeft } from "@coreui/icons";
 
 import { QrReader } from "react-qr-reader";
 
 const Scan = () => {
   let [qrResponse, setQrResponse] = useState('No Response');
 
+  function handleGoBack() {
+    window.location.href = '/#/queue';
+  }
+
   return (
-    <CContainer>
+    <CContainer style={{fontFamily: 'Poppins', display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+      <CIcon onClick={handleGoBack} style={{border: '1px solid #000', marginRight: '10px'}} icon={cilArrowLeft} height={36} width={36} size="custom-size"></CIcon>
       <QrReader
         onResult={(result, error) => {
           if (!!result) {
