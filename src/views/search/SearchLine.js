5d80bfe32bae706df71e58258fcb9f509dfe7342
ssimiao <sssimiao10@gmail.com>
2022-11-18 22:08:33 -0300

    usuario ja consegue fazer reserva

--- src/views/search/SearchLine.js
+++ src/views/search/SearchLine.js
@@ -30,6 +30,45 @@ const Range = () => {
       e.stopPropagation();      
     }
     setValidated(true);
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    const address = data.local.value;
+
+    fetch("http://shortline-app.herokuapp.com/queues?address=" + address, {
+      method: 'GET',
+      headers: myHeaders
+    })
+    .then(res => {
+      if(res.ok) {
+        res.json().then(jsonQueue => {
+          let body = JSON.stringify({
+            idQueue: jsonQueue.id,
+            idUser: localStorage.getItem("userId")  
+          })
+          fetch("http://shortline-app.herokuapp.com/reserves", {
+            method: 'POST',
+            body: body,
+            headers: myHeaders
+          })
+          .then(res => {
+            if(res.ok) {
+              window.location.href = "/#/mainmenu"
+            }
+          })
+          .catch(e => {
+            console.log(e)
+          }).finally(() => {
+            console.log("finalizou reserva");
+          })          
+        }) 
+      }
+    })
+    .catch(e => {
+      console.log(e)
+    }).finally(() => {
+      console.log("buscou queue");
+    })
   }
 
     return (
@@ -48,7 +87,7 @@ const Range = () => {
                 <CInputGroupText>
                   <CIcon icon={cilMap} />
                 </CInputGroupText>
-                <CFormInput id="map-autocomplete" placeholder="Selecione o endereço do estabelecimento" autoComplete="username" required feedbackInvalid="Por favor, informe um endereço."/>
+                <CFormInput name="local" id="map-autocomplete" placeholder="Selecione o endereço do estabelecimento" autoComplete="username" required feedbackInvalid="Por favor, informe um endereço."/>
               </CInputGroup>
               <div className="mb-3">
                 <Map id="map" height="400px"></Map>
